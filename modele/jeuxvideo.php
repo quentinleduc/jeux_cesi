4a433f5f8c022a07308dcdb2b87a940377cefd23
Racar79 <gpapet79@gmail.com>
2017-10-10 14:28:35 +0200

    Jeuxivdeo
    Ajout de l'objet jeuxvideo

---
+++ modele/jeuxvideo.php
@@ -0,0 +1,73 @@
+<?php
+
+class JeuxVideo {
+	private $id;
+	private $nom;
+	private $categorie_id;
+	private $typejeux_id;
+	private $dateinsertion;
+	private $datemaj;
+
+
+
+	function __construct($i, $n, $p, $l, $m, $e){
+		$this->id = $i;
+		$this->nom = $n;
+		$this->categorie_id = $p;
+		$this->typejeux_id = $l;
+		$this->dateinsertion = $m;
+		$this->datemaj = $e;
+	}
+
+	public function get_nom(){
+		return $this->nom;
+	}
+
+	public function set_nom($n){
+		$this->nom = $n;
+	}
+
+	public function get_categorie_id(){
+		return $this->categorie_id;
+	}
+
+	public function set_categorie_id($s){
+		$this->categorie_id = $s;
+	}
+
+	public function get_typejeux_id(){
+		return $this->typejeux_id;
+	}
+
+	public function set_typejeux_id($n){
+		$this->typejeux_id = $n;
+	}
+
+	public function get_dateinsertion(){
+		return $this->dateinsertion;
+	}
+
+	public function set_dateinsertion($n){
+		$this->dateinsertion = $n;
+	}
+
+	public function get_datemaj(){
+		return $this->datemaj;
+	}
+
+	public function set_datemaj($n){
+		$this->datemaj = $n;
+	}
+
+	public function get_id(){
+		return $this->id;
+	}
+
+	public function set_id($n){
+		$this->id = $n;
+	}
+
+}
+
+
+?>
