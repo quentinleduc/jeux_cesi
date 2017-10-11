979663cb909c7a8cde754c2a309239737dec7f80
Racar79 <gpapet79@gmail.com>
2017-10-11 17:21:34 +0200

    Modi login

--- login1.php
+++ login1.php
@@ -1,15 +1,21 @@
 <?php
 session_start();
 error_reporting(E_ALL & ~E_NOTICE);
+require_once __DIR__."/modele/DAO_user.php";
+	 $dao= new DAO_user;
+	 $list = $dao->get_user($_POST["login"],$_POST["mdp"]);
 
-$DAO = New DAO();
-$Identifiant = $DAO->get_user($_POST["login"],$_POST["mdp"]);
+echo $_POST["login"];
+echo $_POST["mdp"];
 
-if($_POST["login"] == $Identifiant.get_login && $_POST["mdp"] == $Identifiant.get_mdp()){
+echo $list->get_login();
+echo $list->get_mdp();
+
+if($_POST["login"] == $list.get_login()&& $_POST["mdp"] == $list.get_mdp()){
   $_SESSION["login"] = $_POST["login"];
   $_SESSION["mdp"] = $_POST["mdp"];
 
-  header("location:home.php");
+ // header("location:home.php");
 }
 
 else {
