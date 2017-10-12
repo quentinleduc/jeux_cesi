6fa4a5bbbf3805d76ce10d640e1a9ef228c42a77
youKnow44 <yasscomoko@gmail.com>
2017-10-12 14:23:02 +0200

    fichier de test

--- test.php
+++ test.php
@@ -1,12 +1,16 @@
 <?php
 
 require_once __DIR__."/modele/DAO_user.php";
-	 $dao= new DAO_user();
-	 $list = $dao->get_all_users();
 
-	 for($i=0;$i<count($list);$i++){
+require_once __DIR__."/modele/DAO_jeuxVideo.php";
+
+	 $dao= new DAO_jeuxVideo();
+	 $list = $dao->get_jeu(2);
+	 echo "nom img ".$list->get_img();
+
+	/* for($i=0;$i<count($list);$i++){
       echo "test ".$list[$i]->get_nom() ." , le mot de passe : ".md5($list[$i]->get_mdp()). ", mot de passe non crypté : ".$list[$i]->get_mdp();
-    }
+    }*/
 	;
 
 ?>
